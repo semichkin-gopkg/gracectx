9d3a49a62d3054f1108151eeb7cc3d9f10b15828
bulat <bulat@collabox.dev>
2024-05-05 15:40:15 +0300

    fixes

--- gracectx.go
+++ gracectx.go
@@ -2,25 +2,14 @@ package gracectx
 
 import (
 	"context"
-	"os"
 	"os/signal"
 	"syscall"
 )
 
-func New(parent context.Context) (context.Context, context.CancelFunc) {
-	ctx, cancel := context.WithCancel(parent)
-
-	ch := make(chan os.Signal, 1)
-	go func() {
-		signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
-		<-ch
-		cancel()
-	}()
-
-	return ctx, cancel
+func From(parent context.Context) (context.Context, context.CancelFunc) {
+	return signal.NotifyContext(parent, syscall.SIGINT, syscall.SIGTERM)
 }
 
-func Background() context.Context {
-	ctx, _ := New(context.Background())
-	return ctx
+func New() (context.Context, context.CancelFunc) {
+	return From(context.Background())
 }
